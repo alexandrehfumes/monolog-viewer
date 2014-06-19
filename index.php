293bf29fc10c0c3d10f4b0cf6dc5f25bb5ea4bb3
Donovan Tengblad <purplefish32@gmail.com>
2014-06-19 17:20:58 +0200

    Lots of changes

--- index.php
+++ index.php
@@ -3,13 +3,13 @@
 
     session_start();
 
-
     define('APP_PATH', __DIR__ . '/app/');
     define('VENDOR_PATH', __DIR__ . '/vendor/');
     define('BASE_URL', 'http://' . $_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI']);
     define('WEB_URL', BASE_URL . 'web/');
 
     require_once VENDOR_PATH . "autoload.php";
+    require_once __DIR__ . '/web/password.php';
 
     $app = new \Slim\Slim(array(
         'view' => new \Slim\Views\Twig()
@@ -25,59 +25,100 @@
         new \Slim\Views\TwigExtension(),
     );
 
-    $app->get('/clients/:client/', function ($client) use ($app) {
-        $app->render('index.html.twig');
-    });
+    $app->get('/', function () use ($app) {
+        echo('home');
+    })->name('home');
 
-    $app->get('/clients/:client/logs/:log', function ($client, $log) {
-        echo "Client : $client, Log : $log";
-    });
+    $app->get('/clients/:clientSlug/:logSlug', function ($clientSlug, $logSlug) use ($app) {
+        $viewer = new \SyonixLogViewer\LogViewer(APP_PATH . 'config/config.json');
+
+        print_r($viewer);
+
+        //TODO get log
+        $log = null;
+
+
+        // $data = new \stdClass();
+
+        // /* Load Logfiles */
+        // //if($_SESSION['authenticated'] === true && $viewer->hasLogs()) {
+        // if($viewer->hasLogs()) {
+        //     if(isset($client) && $viewer->clientExists($client)) $data->client = $client;
+
+        //     if(isset($log) && $viewer->logExists($client, $log)) $data->log = $log;
+
+        //     if(!isset($client)) {
+        //         $data->client = $viewer->getFirstClient();
+        //     }
+
+        //     if(!isset($log)) {
+        //         $data->log = $viewer->getFirstLog($data->client);
+        //     }
+        // } else {
+        //   //TODO there are no logs
+        // }
+
+        $app->render('index.html.twig', array(
+            'viewer'     => $viewer,
+            'log'        => $log,
+            'clientSlug' => $clientSlug,
+            'logSlug'    => $logSlug
+        ));
+    })->name('log');
 
     $app->get('/login', function () use ($app) {
         $app->render('login.html.twig');
-    });
+    })->name("login");
 
-    $app->get('/install', function () use ($app) {
-        $app->render('install.html.twig');
+    $app->post('/login', function () use ($app) {
+        //TODO if logged in
+        $app->redirect(
+            $app->urlFor('home')
+        );
     });
 
     $app->get('/logout', function () use ($app) {
-        $_SESSION['authenticated'] = false;
         session_destroy();
-        $app->redirect('/login');
+        $app->redirect('login');
+    })->name("logout");
+
+    $app->get('/install', function () use ($app) {
+        if(!is_dir('./secure/pwd')) {
+          $app->render('install.html.twig');
+        }
+        else {
+          $app->redirect(
+            $app->urlFor('login')
+          );
+        }
+    });
+
+    $app->post('/install', function () use ($app) {
+        if($app->request()->post('password') == $app->request()->post('password-repeat')) {
+            if(mkdir('./secure/pwd', 0700)) {
+                file_put_contents('./secure/pwd/'.uniqid(), password_hash($_POST['password'], PASSWORD_DEFAULT));
+            } else {
+                //TODO this never happens
+                $app->flash('error', 'Could not save the password. Please make sure that the directory <code>/secure</code> is writable.');
+            }
+        } else {
+            $app->flash('error', 'The provided Passwords do not match.');
+        }
+
+        $app->render('install.html.twig');
     });
 
     $app->run();
 
     die();
 
-    require_once __DIR__ . '/web/password.php';
 
 
     date_default_timezone_set('Europe/Paris');
     setlocale(LC_ALL, 'fr_FR.utf8');
 
-    $setupNomatch = false;
-    $setupFailed = false;
-    $setupSuccessful = false;
-
-    $viewer = new \SyonixLogViewer\LogViewer(APP_PATH . 'config/config.json');
 
 
-    /* Setup login */
-    if(isset($_POST['setup'])) {
-        if($_POST['password'] == $_POST['password-repeat']) {
-            if(is_dir('./secure/pwd')) rmdir('./secure/pwd');
-            if(mkdir('./secure/pwd', 0700)) {
-                file_put_contents('./secure/pwd/'.uniqid(), password_hash($_POST['password'], PASSWORD_DEFAULT));
-                $setupSuccessful = true;
-            } else {
-                $setupFailed = true;
-            }
-        } else {
-            $setupNomatch = true;
-        }
-    }
 
     /* Check Passwd File */
     $checkPasswdFile = false;
@@ -98,23 +139,5 @@
             $_SESSION['authenticated'] = true;
         } else {
             $_SESSION['authenticated'] = false;
-        }
-    }
-
-    /* Load Logfiles */
-    if($_SESSION['authenticated'] === true && $viewer->hasLogs()) {
-        $redirect = false;
-        if(isset($_GET['c']) && $viewer->clientExists($_GET['c'])) $_SESSION['client'] = $_GET['c'];
-        if(isset($_GET['l']) && $viewer->logExists($_GET['c'], $_GET['l'])) $_SESSION['log'] = $_GET['l'];
-        if(!isset($_GET['c'])) {
-            $_SESSION['client'] = $viewer->getFirstClient();
-            $redirect = true;
-        }
-        if(!isset($_GET['l'])) {
-            $_SESSION['log'] = $viewer->getFirstLog($_SESSION['client']);
-            $redirect = true;
-        }
-        if($redirect === true) {
-            header("Location: " . BASE_URL . $_SESSION['client'] . "/" . $_SESSION['log']);
         }
     }
